b194c3a7cadd027fcb7c66bcf0daf548f60f7834
neuxdotdev <neuxdev1@gmail.com>
2026-04-26 20:52:48 +0700

    chore(cleanup): remove legacy global info module
    Delete src/cmd/global/info.rs containing
    show_info function with version and config data.
    
    Info functionality moved to database::info
    module with extended table count display.
    
    Centralizes info logic; improves testability
    of configuration reporting.
    
    Refs: none
    
    Test: neuxdb database info shows table count
    
    BREAKING CHANGE: NONE

--- src/cmd/global/info.rs
+++
@@ -1,9 +0,0 @@
-
-use crate::config::{DATA_DIR, TABLE_EXT};
-pub fn show_info() {
-    println!("NeuxDB Information:");
-    println!("  Version:     0.1.0");
-    println!("  Data Dir:    {}", DATA_DIR);
-    println!("  Table Ext:   .{}", TABLE_EXT);
-    println!("  Delimiter:   '|'");
-}
